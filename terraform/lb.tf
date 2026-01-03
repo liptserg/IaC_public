3174efa86229a919a4d524834398af17904bdadd
liptserg <38787405+liptserg@users.noreply.github.com>
2026-01-03 21:13:29 +0300

    Update lb.tf

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -26,10 +26,19 @@ resource "yandex_lb_network_load_balancer" "wp_lb" {
     target_group_id = yandex_lb_target_group.wp_tg.id
 
     healthcheck {
-      name = "http"
-      http_options {
+    #   name = "http"
+    #   http_options {
+    #     port = 80
+    #     path = "/"
+    #   }
+    #   interval            = 2
+    #   timeout             = 1
+    #   unhealthy_threshold = 2
+    #   healthy_threshold   = 2
+    # }
+      name = "tcp"
+      tcp_options {
         port = 80
-        path = "/"
       }
       interval            = 2
       timeout             = 1
